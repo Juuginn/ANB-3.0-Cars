087ad9d1d3ac35b013c9cd147b9f3cacf4c39517
Juuginn <tbgamingonly@gmail.com>
2023-05-01 05:41:54 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    zlay_asap/__resource.lua
    	deleted:    zlay_asap/common/Read Me
    	renamed:    zlay_asap/carcols.meta -> zlay_asap/common/carcols.meta
    	renamed:    zlay_asap/carvariations.meta -> zlay_asap/common/carvariations.meta
    	renamed:    zlay_asap/handling.meta -> zlay_asap/common/handling.meta
    	renamed:    zlay_asap/vehicles.meta -> zlay_asap/common/vehicles.meta
    	new file:   zlay_asap/fxmanifest.lua

---
+++ zlay_asap/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+    'common/vehicles.meta',
+    'common/carvariations.meta',
+    'common/carcols.meta',
+    'common/handling.meta',    
+	'common/dlctext.meta',	
+}
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
+data_file 'DLC_TEXT_FILE' 'common/dlctext.meta'
