e7934189d0d8f3e959cf21bc8f27fb032297d472
Juugin <113743185+Juuginn@users.noreply.github.com>
2023-05-01 05:27:14 +0800

    Add files via upload

---
+++ hycader34/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+	'common/carcols.meta',
+	'common/carvariations.meta',
+	'common/handling.meta',
+	'common/vehicles.meta',
+}
+
+data_file 'HANDLING_FILE' 'common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/carvariations.meta'
